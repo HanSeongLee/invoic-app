32a0b5aef4325948a0ac535dc23d2051df12bcfc
HanSeong Lee <jkhanseong@gmail.com>
2022-04-12 19:59:57 +0900

    ADD | Inherit InputHTMLAttributes

--- components/InputFieldController/index.tsx
+++ components/InputFieldController/index.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, {InputHTMLAttributes} from 'react';
 import {Controller} from "react-hook-form";
 import InputField from "../InputField";
 import {Control} from "react-hook-form/dist/types/form";
 
-interface IProps {
+interface IProps extends InputHTMLAttributes<HTMLInputElement> {
     control: Control;
-    id: string;
+    id?: string;
     name: string;
     type: string;
 };
 
-const InputFieldController: React.FC<IProps> = ({ control, id, name, type, children }) => {
+const InputFieldController: React.FC<IProps> = ({
+                                                    control, id, name, type, children,
+                                                    ...props
+                                                }) => {
     return (
         <Controller control={control}
                     name={name}
-                    render={({ field }) => (
+                    render={({field}) => (
                         <InputField type={type}
                                     id={id}
+                                    {...props}
                                     {...field}
                         >
                             {children}
