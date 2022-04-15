24fbe1d8ca1d805fe39b6ce551bb9bd13648a5cf
HanSeong Lee <jkhanseong@gmail.com>
2022-04-15 21:19:44 +0900

    ADD | Shadow on FixedButtonContainer

--- components/FixedButtonContainer/index.tsx
+++ components/FixedButtonContainer/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import styles from './style.module.scss';
 import Container from "../Container";
+import cn from "classnames";
 
-const FixedButtonContainer: React.FC = ({ children }) => {
+interface IProps {
+    shadow?: boolean;
+};
+
+const FixedButtonContainer: React.FC = ({ shadow, children }) => {
     return (
-        <div className={styles.fixedButtonContainer}>
+        <div className={cn(styles.fixedButtonContainer, {
+            [styles.shadow]: shadow,
+        })}>
             <Container className={styles.container}>
                 {children}
             </Container>
         </div>
     );
+};
+
+FixedButtonContainer.defaultProps = {
+    shadow: false,
 };
 
 export default FixedButtonContainer;
